4c094970018fb2b41f256624d54014eb1e8dfcf8
Xinran Liu <angelaliu20011005@gmail.com>
2021-10-07 15:23:00 -0400

    a4 homework

--- assignments/a4-euler/unique.cpp
+++ assignments/a4-euler/unique.cpp
@@ -1,7 +1,93 @@
+#include "atkmath/vector3.h"
+#include "atkmath/matrix3.h"
+#include "atkmath/quaternion.h"
 #include "atkui/framework.h"
+#include <iostream>
+
+using glm::vec3;
+using atkmath::Matrix3;
+using atkmath::Vector3;
+using atkmath::Quaternion;
 using namespace glm;
 
+class A4Unique : public atkui::Framework {
+   public:
+      A4Unique() : atkui::Framework(atkui::Orthographic) {
+   }
+
+   virtual void setup() {
+      pos = vec3(0);
+      positionX = width()/2; // set origin
+      positionY = height()/2; // set origin
+      distance = 50.0; //set distance between each circle
+      radius = 0.0; //distance from the center
+      theta = 0.0; // circulation movement
+      degree = acos(0.0)/9.0f; //10 degree (for spacing each triangle)
+      size = 20.0f; // modify the size of the cone
+      rotationAngle = 0.0f; //this is the angle that the cone is going to rotate
+      rotationRate = 0.01f;
+      thetaRate = 0.001f; //rate the triangle is doing circular motion
+  }
+
+   virtual void scene() {
+      float px; //recording the x position of the triangle(cone)
+      float py; //recording the y position of the triangle
+      for (int i = 0; i < 6; i++){
+         //alternate color
+         if ((i%5)==0)
+            setColor(vec3(0,165,227)/255.0f);
+         if ((i%5)==1)
+            setColor(vec3(141,215,191)/255.0f);
+         if ((i%5)==2)
+            setColor(vec3(255,150,197)/255.0f);
+         if ((i%5)==3)
+            setColor(vec3(255,87,104)/255.0f);
+         if ((i%5)==4)
+            setColor(vec3(255,162,58)/255.0f);
+         radius = i * distance;
+         for (int j = 0; j < i * 9; j++){
+            degree = (acos(0.0) * 4.0f) / (i * 9.0f);
+            if(i%2){ 
+               px = radius * sin(theta + j * degree) + positionX;
+               py = radius * cos(theta + j * degree) + positionY;
+            } else {
+               px = radius * cos(theta + j * degree) + positionX;
+               py = radius * sin(theta + j * degree) + positionY;
+            }
+            rotationAngle += rotationRate * dt();
+            theta -= i * thetaRate * dt(); 
+            push();
+            translate(vec3(px, py, 0)); 
+            // translate the position for each triangle as origin  
+            rotate(acos(0.0), vec3(0,1,0)); 
+            // we want the side view of the cone, since we want triangle
+            rotate(rotationAngle, vec3(1,0,0)); 
+            // rotating alone x-axis 
+            scale(vec3(0, 2, 1));
+            // size change
+            drawCone(vec3(0), size);
+            pop();
+         }    
+      }
+  }
+
+ private:
+   vec3 pos;
+   float radius;
+   float distance;
+   float positionX;
+   float positionY;
+   float theta;
+   float degree;
+   float size;
+   float rotationAngle;
+   float rotationRate;
+   float thetaRate;
+};
+
 int main(int argc, char** argv) {
+   A4Unique viewer;
+   viewer.run();
    return 0;
 }
 
--- libsrc/atkmath/matrix3-basecode.cpp
+++ libsrc/atkmath/matrix3-basecode.cpp
@@ -4,63 +4,182 @@
 namespace atkmath {
 
 Vector3 Matrix3::toEulerAnglesXYZ() const
-{
-   return Vector3();
+{     
+   float x = atan2(-mM[1][2], mM[2][2]);
+   float y = asin(mM[0][2]);
+   float z = atan2(-mM[0][1], mM[0][0]);
+   if(mM[0][2] == 1 || mM[0][2] == -1){
+      x = 0.0f;
+      z = acos(mM[1][1]);
+   }
+   return Vector3(x, y, z);
 }
 
 Vector3 Matrix3::toEulerAnglesXZY() const
 {
-   return Vector3();
+   float x = atan2(mM[2][1], mM[1][1]);
+   float y = atan2(mM[0][2], mM[0][0]);
+   float z = asin(-mM[0][1]);
+   if (mM[0][1] == 1 || mM[0][1] == -1){
+      x = acos(mM[2][2]);
+      y = 0.0f;
+   }
+   return Vector3(x, y, z);
 }
 
 Vector3 Matrix3::toEulerAnglesYXZ() const
 {
-   return Vector3();
+   float x = asin(-mM[1][2]);
+   float y = atan2(mM[0][2], mM[2][2]);
+   float z = atan2(mM[1][0], mM[1][1]);
+   if(mM[1][2] == 1 || mM[1][2] == -1){
+      y = atan2(-mM[2][0], mM[0][0]);
+      z = 0.0f;
+   }
+   return Vector3(x, y, z);
 }
 
 Vector3 Matrix3::toEulerAnglesYZX() const
 {
-   return Vector3();
+   float x = atan2(-mM[1][2], mM[1][1]);
+   float y = atan2(-mM[2][0], mM[0][0]);
+   float z = asin(mM[1][0]);
+   if(mM[1][0] == 1 || mM[1][0] == -1){
+      x = acos(mM[2][2]);
+      y = 0.0f;
+   }
+   return Vector3(x, y, z);
 }
 
 Vector3 Matrix3::toEulerAnglesZXY() const
 {
-   return Vector3();
+   float x = asin(mM[2][1]);
+   float y = atan2(-mM[2][0], mM[2][2]);
+   float z = atan2(-mM[0][1], mM[1][1]);
+   if(mM[2][1] == 1 || mM[2][1] == -1){
+      y = 0.0f;
+      z = atan2(mM[1][0], mM[0][0]);
+   }
+   return Vector3(x, y, z);
 }
 
 Vector3 Matrix3::toEulerAnglesZYX() const
-{
-   return Vector3();
+{  
+   float x = atan2(mM[2][1], mM[2][2]);
+   float y = asin(-mM[2][0]);
+   float z = atan2(mM[1][0], mM[0][0]);
+
+   if(mM[2][0] == 1 || mM[2][0] == -1){
+      x = 0.0f;
+      z = acos(mM[1][1]);
+   }
+   return Vector3(x, y, z);
 }
 
 void Matrix3::fromEulerAnglesXYZ(const Vector3& angleRad)
-{
-   *this = Identity;
+{  
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   Matrix3 Rx = Matrix3(1, 0, 0, 
+                        0, cos(x), -sin(x), 
+                        0, sin(x), cos(x));
+   Matrix3 Ry = Matrix3(cos(y), 0, sin(y), 
+                        0, 1, 0, 
+                        -sin(y), 0, cos(y));
+   Matrix3 Rz = Matrix3(cos(z), -sin(z), 0, 
+                        sin(z), cos(z), 0, 
+                        0, 0, 1);
+   *this = Rx * Ry * Rz;
 }
 
 void Matrix3::fromEulerAnglesXZY(const Vector3& angleRad)
-{
-   *this = Identity;
+{  
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   Matrix3 Rx = Matrix3(1, 0, 0, 
+                        0, cos(x), -sin(x), 
+                        0, sin(x), cos(x));
+   Matrix3 Ry = Matrix3(cos(y), 0, sin(y), 
+                        0, 1, 0, 
+                        -sin(y), 0, cos(y));
+   Matrix3 Rz = Matrix3(cos(z), -sin(z), 0, 
+                        sin(z), cos(z), 0, 
+                        0, 0, 1);
+
+   *this = Rx * Rz * Ry;
 }
 
 void Matrix3::fromEulerAnglesYXZ(const Vector3& angleRad)
-{
-   *this = Identity;
+{    
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   Matrix3 Rx = Matrix3(1, 0, 0, 
+                        0, cos(x), -sin(x), 
+                        0, sin(x), cos(x));
+   Matrix3 Ry = Matrix3(cos(y), 0, sin(y), 
+                        0, 1, 0, 
+                        -sin(y), 0, cos(y));
+   Matrix3 Rz = Matrix3(cos(z), -sin(z), 0, 
+                        sin(z), cos(z), 0, 
+                        0, 0, 1);
+   *this = Ry * Rx * Rz;
 }
 
 void Matrix3::fromEulerAnglesYZX(const Vector3& angleRad)
-{
-   *this = Identity;
+{  
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   Matrix3 Rx = Matrix3(1, 0, 0, 
+                        0, cos(x), -sin(x), 
+                        0, sin(x), cos(x));
+   Matrix3 Ry = Matrix3(cos(y), 0, sin(y), 
+                        0, 1, 0, 
+                        -sin(y), 0, cos(y));
+   Matrix3 Rz = Matrix3(cos(z), -sin(z), 0, 
+                        sin(z), cos(z), 0, 
+                        0, 0, 1);
+
+   *this = Ry * Rz * Rx;
 }
 
 void Matrix3::fromEulerAnglesZXY(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   Matrix3 Rx = Matrix3(1, 0, 0, 
+                        0, cos(x), -sin(x), 
+                        0, sin(x), cos(x));
+   Matrix3 Ry = Matrix3(cos(y), 0, sin(y), 
+                        0, 1, 0, 
+                        -sin(y), 0, cos(y));
+   Matrix3 Rz = Matrix3(cos(z), -sin(z), 0, 
+                        sin(z), cos(z), 0, 
+                        0, 0, 1);
+
+   *this = Rz * Rx * Ry;
 }
 
 void Matrix3::fromEulerAnglesZYX(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   Matrix3 Rx = Matrix3(1, 0, 0, 
+                        0, cos(x), -sin(x), 
+                        0, sin(x), cos(x));
+   Matrix3 Ry = Matrix3(cos(y), 0, sin(y), 
+                        0, 1, 0, 
+                        -sin(y), 0, cos(y));
+   Matrix3 Rz = Matrix3(cos(z), -sin(z), 0, 
+                        sin(z), cos(z), 0, 
+                        0, 0, 1);
+
+   *this = Rz * Ry * Rx;
 }
 
 void Matrix3::toAxisAngle(Vector3& axis, double& angleRad) const
